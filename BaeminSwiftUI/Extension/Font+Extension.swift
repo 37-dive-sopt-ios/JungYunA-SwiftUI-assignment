910fc22dc332b3d7cf26ebf7bea11cea7aff3f52
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-12-13 00:43:53 +0900

    feat: font extension 추가

---
+++ BaeminSwiftUI/Extension/Font+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Font+Extension.swift
+//  BaeminSwiftUI
+//
+//  Created by 정윤아 on 12/13/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let bold16: Font = .custom("Pretendard-Bold", size: 16)
+    static let regular14: Font = .custom("Pretendard-Regular", size: 14)
+    static let bold18: Font = .custom("Pretendard-Bold", size: 18)
+    static let regular12: Font = .custom("Pretendard-Regular", size: 12)
+    static let bold13: Font = .custom("Pretendard-Bold", size: 13)
+}
